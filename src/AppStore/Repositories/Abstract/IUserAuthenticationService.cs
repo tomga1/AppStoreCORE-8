33358a0f20612fa40aeec819bdd9e1ff52cce32b
tomga1 <tomasgarcia003@gmail.com>
2024-10-09 16:47:04 -0300

    push

---
+++ src/AppStore/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,9 @@
+using AppStore.Models.Domain;
+using AppStore.Models.DTO;
+namespace AppStore.Repositories.Abstract;
+
+
+public interface IUserAuthenticationService
+{
+    
+}
--- src/AppStore/obj/Debug/net8.0/AppStore.AssemblyInfo.cs
+++ src/AppStore/obj/Debug/net8.0/AppStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0897e581143b7aca8a68d1b8e3e5a7498e193d44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa22b462b5505c115113f59fcb24e8e16c195f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
