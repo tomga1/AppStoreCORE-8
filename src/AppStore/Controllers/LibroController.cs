10bb13d5810ac751c581e35f2dfd7898a7895380
tomga1 <tomasgarcia003@gmail.com>
2024-10-15 10:24:26 -0300

    hice un push

--- src/AppStore/Controllers/LibroController.cs
+++ src/AppStore/Controllers/LibroController.cs
@@ -2,9 +2,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace AppStore.Controllers;
 using AppStore.Models.Domain;
 using AppStore.Repositories.Abstract;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
+
+
+[Authorize]
 public class LibroController : Controller
 {
     private readonly ILibroService _libroService; 
@@ -66,8 +71,51 @@ public class LibroController : Controller
 
     public IActionResult Edit(int id)
     {
-        return View();
+        var libro = _libroService.GetById(id);
+        var categoriasLibro = _libroService.GetCategoriaByLibroId(id);
+        var multiSelectListCategorias = new MultiSelectList(_categoriaService.List(),"Id", "Nombre", categoriasLibro);
+
+
+        libro.MultiCategoriasList = multiSelectListCategorias;
+        return View(libro);
     }
+
+    [HttpPost]
+    public IActionResult Edit(Libro libro)
+    {
+        var categoriasLibro = _libroService.GetCategoriaByLibroId(libro.Id);
+        var multiSelectListCategorias = new MultiSelectList(_categoriaService.List(),"Id", "Nombre", categoriasLibro);
+        libro.MultiCategoriasList = multiSelectListCategorias;
+
+        if(!ModelState.IsValid)
+        {
+            return View(libro);
+        }
+
+        if(libro.ImageFile != null)
+        {
+            var fileResultado = _fileService.SaveImage(libro.ImageFile);
+            if(fileResultado.Item1 == 0)
+            {
+                TempData["msg"] = "La imagen no fue guardada";
+                return View(libro);
+            }
+            var imagenName = fileResultado.Item2;
+            libro.Imagen = imagenName; 
+        }
+
+        var resultadoLibro = _libroService.Update(libro);
+        if(!resultadoLibro)
+        {
+            TempData["msg"] = "Errores, no se pudo actualizar el libro";
+            return View(libro);
+        }
+
+        TempData["msg"] = "Se actualizo exitosamente el libro";
+        return View(libro); 
+
+    }
+
 
     public IActionResult LibroList()
     {
@@ -77,6 +125,9 @@ public class LibroController : Controller
 
     public IActionResult Delete(int id)
     {
+        _libroService.Delete(id);
+
+
         return RedirectToAction(nameof(LibroList));
     }
 
